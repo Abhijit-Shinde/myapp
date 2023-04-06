1001c220fe42cc741444451269f0ae0623b2af18
Abhijit Shinde <Abhijit.Shinde@blueconchtech.com>
2023-04-06 17:24:22 +0530

    BookProject - local persistence with API

--- src/BookProject/App.js
+++ src/BookProject/App.js
@@ -1,11 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from "./BookCreate";
 import BookList from "./BookList";
+import axios from "axios";
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  const deleteBook = (id) => {
+  const fetchBooks = async () => {
+    let response = await axios.get("http://localhost:3001/books");
+
+    setBooks(response.data);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+  //https://codepen.io/sgrider/pen/BarEowz?editors=0011
+
+  const deleteBook = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
@@ -13,10 +26,14 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const editBook = (id, newTitle) => {
+  const editBook = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
       return book;
     });
@@ -24,11 +41,12 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const handleCreateBook = (title) => {
-    const book = [
-      ...books,
-      { id: Math.round(Math.random() * 9999), title: title },
-    ];
+  const handleCreateBook = async (title) => {
+    let response = await axios.post("http://localhost:3001/books", {
+      title, //as key value are same so specify once
+    });
+
+    const book = [...books, response.data];
     setBooks(book);
   };
 
